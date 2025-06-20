f5bf6009e7d8d2440f159ea21ba82f3e41fad9c8
Kartik Kallur <karthikskallur@gmail.com>
2025-06-20 14:03:33 +0530

    Add files via upload

---
+++ script/javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let main = document.getElementById("main-content");
+    main.style.opacity = "1";
+    main.style.transform = "translateY(0)";
+});
+
